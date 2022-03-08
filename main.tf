d6a228cf3d111439cd504f70b61f229e8d219e3d
SreeKalyan <51774069+sreekalyanthoughtworks@users.noreply.github.com>
2022-03-08 07:44:38 +0530

    Format the code

--- main.tf
+++ main.tf
@@ -1,133 +1,133 @@
 provider "aws" {
-	  region = "eu-central-1"
+  region = "eu-central-1"
 }
 
-variable vpc_cidr_block {}
-variable subnet_1_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
-variable instance_type {}
-variable ssh_key {}
-variable my_ip {}
+variable "vpc_cidr_block" {}
+variable "subnet_1_cidr_block" {}
+variable "avail_zone" {}
+variable "env_prefix" {}
+variable "instance_type" {}
+variable "ssh_key" {}
+variable "my_ip" {}
 
 data "aws_ami" "amazon-linux-image" {
-	  most_recent = true
-	    owners      = ["amazon"]
+  most_recent = true
+  owners      = ["amazon"]
 
-	      filter {
-		          name   = "name"
-			      values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-			        }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
 
-				  filter {
-					      name   = "virtualization-type"
-					          values = ["hvm"]
-						    }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 output "ami_id" {
-	  value = data.aws_ami.amazon-linux-image.id
+  value = data.aws_ami.amazon-linux-image.id
 }
 
 resource "aws_vpc" "myapp-vpc" {
-	  cidr_block = var.vpc_cidr_block
-	    tags = {
-		          Name = "${var.env_prefix}-vpc"
-			    }
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name = "${var.env_prefix}-vpc"
+  }
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
-	  vpc_id = aws_vpc.myapp-vpc.id
-	    cidr_block = var.subnet_1_cidr_block
-	      availability_zone = var.avail_zone
-	        tags = {
-			      Name = "${var.env_prefix}-subnet-1"
-			        }
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subnet_1_cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+    Name = "${var.env_prefix}-subnet-1"
+  }
 }
 
 resource "aws_security_group" "myapp-sg" {
-	  name   = "myapp-sg"
-	    vpc_id = aws_vpc.myapp-vpc.id
+  name   = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
 
-	      ingress {
-		          from_port   = 22
-			      to_port     = 22
-			          protocol    = "tcp"
-				      cidr_blocks = [var.my_ip]
-				        }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
 
-					  ingress {
-						      from_port   = 8080
-						          to_port     = 8080
-							      protocol    = "tcp"
-							          cidr_blocks = ["0.0.0.0/0"]
-								    }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-								      egress {
-									          from_port       = 0
-										      to_port         = 0
-										          protocol        = "-1"
-											      cidr_blocks     = ["0.0.0.0/0"]
-											          prefix_list_ids = []
-												    }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
 
-												      tags = {
-													          Name = "${var.env_prefix}-sg"
-														    }
+  tags = {
+    Name = "${var.env_prefix}-sg"
+  }
 }
 
 resource "aws_internet_gateway" "myapp-igw" {
-		vpc_id = aws_vpc.myapp-vpc.id
-		    
-		        tags = {
-				     Name = "${var.env_prefix}-internet-gateway"
-				        }
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  tags = {
+    Name = "${var.env_prefix}-internet-gateway"
+  }
 }
 
 resource "aws_route_table" "myapp-route-table" {
-	   vpc_id = aws_vpc.myapp-vpc.id
+  vpc_id = aws_vpc.myapp-vpc.id
 
-	      route {
-		           cidr_block = "0.0.0.0/0"
-			        gateway_id = aws_internet_gateway.myapp-igw.id
-				   }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
 
-				      # default route, mapping VPC CIDR block to "local", created implicitly and cannot be specified.
+  # default route, mapping VPC CIDR block to "local", created implicitly and cannot be specified.
 
-				         tags = {
-						      Name = "${var.env_prefix}-route-table"
-						         }
-							  }
+  tags = {
+    Name = "${var.env_prefix}-route-table"
+  }
+}
 
 # Associate subnet with Route Table
 resource "aws_route_table_association" "a-rtb-subnet" {
-	  subnet_id      = aws_subnet.myapp-subnet-1.id
-	    route_table_id = aws_route_table.myapp-route-table.id
+  subnet_id      = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
 }
 
 resource "aws_key_pair" "ssh-key" {
-	  key_name   = "myapp-key"
-	    public_key = file(var.ssh_key)
+  key_name   = "myapp-key"
+  public_key = file(var.ssh_key)
 }
 
 output "server-ip" {
-	    value = aws_instance.myapp-server.public_ip
+  value = aws_instance.myapp-server.public_ip
 }
 
 resource "aws_instance" "myapp-server" {
-	  ami                         = data.aws_ami.amazon-linux-image.id
-	    instance_type               = var.instance_type
-	      key_name                    = "myapp-key"
-	        associate_public_ip_address = true
-		  subnet_id                   = aws_subnet.myapp-subnet-1.id
-		    vpc_security_group_ids      = [aws_security_group.myapp-sg.id]
-		      availability_zone			      = var.avail_zone
+  ami                         = data.aws_ami.amazon-linux-image.id
+  instance_type               = var.instance_type
+  key_name                    = "myapp-key"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids      = [aws_security_group.myapp-sg.id]
+  availability_zone           = var.avail_zone
 
-		        tags = {
-				    Name = "${var.env_prefix}-server"
-				      }
+  tags = {
+    Name = "${var.env_prefix}-server"
+  }
 
-				        user_data = <<EOF
+  user_data = <<EOF
 					                 #!/bin/bash
 							                  apt-get update && apt-get install -y docker-ce
 									                   systemctl start docker
@@ -137,19 +137,19 @@ resource "aws_instance" "myapp-server" {
 }
 
 resource "aws_instance" "myapp-server-two" {
-	  ami                         = data.aws_ami.amazon-linux-image.id
-	    instance_type               = var.instance_type
-	      key_name                    = "myapp-key"
-	        associate_public_ip_address = true
-		  subnet_id                   = aws_subnet.myapp-subnet-1.id
-		    vpc_security_group_ids      = [aws_security_group.myapp-sg.id]
-		      availability_zone			      = var.avail_zone
+  ami                         = data.aws_ami.amazon-linux-image.id
+  instance_type               = var.instance_type
+  key_name                    = "myapp-key"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids      = [aws_security_group.myapp-sg.id]
+  availability_zone           = var.avail_zone
 
-		        tags = {
-				    Name = "${var.env_prefix}-server-two"
-				      }
+  tags = {
+    Name = "${var.env_prefix}-server-two"
+  }
 
-				        user_data = <<EOF
+  user_data = <<EOF
 					                 #!/bin/bash
 							                  apt-get update && apt-get install -y docker-ce
 									                   systemctl start docker
